28b1347c5fb389fc1ff7b32a5b6998975fbf30b4
ADMAR AUGUSTO CALIXTO JUNIOR <admar.junior@cs.up.edu.br>
2026-05-22 11:09:48 -0300

    Trabalho PHP

---
+++ includes/cabecalho.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Catálogo Gourmet</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+<header>
+
+    <nav class="navbar">
+
+        <div class="logo">
+
+            🍴 Catálogo Gourmet
+
+        </div>
+
+        <ul class="menu">
+
+            <li><a href="index.php">Home</a></li>           
+
+            <li><a href="login.php">Login</a></li>
+
+            <li><a href="protegido.php">Admin</a></li>
+
+        </ul>
+
+    </nav>
+
+</header>
---
+++ includes/rodape.php
@@ -0,0 +1,10 @@
+<footer>
+
+    <h3>Catálogo Gourmet © 2026</h3>
+
+    <p>As melhores receitas da internet.</p>
+
+</footer>
+
+</body>
+</html>
